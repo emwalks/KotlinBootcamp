feabc0ac4253d66a4241e1272352b6851af6ac75
Emma Walker <walkere@tcd.ie>
2022-01-03 18:15:26 +0000

    uses kotlins higher order func with()

--- src/main/kotlin/example/simplelambdas/Lambdas.kt
+++ src/main/kotlin/example/simplelambdas/Lambdas.kt
@@ -11,8 +11,19 @@ fun main() {
     val myFish = listOf<Fish>(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
     val resultOfFilter = myFish.filter { it.name.contains("i") }.joinToString(", ") { it.name }
     println("${resultOfFilter.trim()}")
+
+    fishExamples()
 }
 
 data class Fish(val name: String)
+
+// https://developer.android.com/codelabs/kotlin-bootcamp-sams#4
+
+fun fishExamples() {
+    val fish = Fish("splashy")
+    with(fish.name) {
+        println(this.capitalize())
+    }
+}
 
 

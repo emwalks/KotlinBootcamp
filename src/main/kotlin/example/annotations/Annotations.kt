494aed24513e54ce1e6366548b2dbb29957310df
Emma Walker <walkere@tcd.ie>
2022-01-03 17:53:30 +0000

    adds annotations file but reflect import not working correctly

--- src/main/kotlin/example/annotations/Annotations.kt
+++ src/main/kotlin/example/annotations/Annotations.kt
@@ -1,20 +1,26 @@
 package example.annotations
 
-import example.myapp.plant
+// reflection import not working as expected
+// import kotlin.reflect.full.*
 
-class Annotations {
-}
 
 class Plant {
     fun trim() {}
     fun fertilise() {}
 
-    fun printAllFunctions(vararg declaredFunction: () -> Unit ) {
-        println("Functions in class include $declaredFunction")
+//    fun printAllFunctions(vararg declaredFunction: () -> Unit ) {
+//        println("Functions in class include $declaredFunction")
+//    }
+}
+
+fun testAnnotations() {
+    //  Use ::class to get information about a class at runtime.
+    val classObj = Plant::class
+  //  for (m in classObj.declaredMemberFunctions) {
+   //     println(m.name)
     }
 }
 
 fun main () {
-    val plant = Plant()
-    plant.printAllFunctions({ plant.trim() }, { plant.fertilise() })
+    testAnnotations()
 }

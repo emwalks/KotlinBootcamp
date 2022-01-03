881e62611adce3029296e18bf45f118ceb7c2b1d
Emma Walker <walkere@tcd.ie>
2022-01-03 18:09:28 +0000

    adds filter and join to string lambdas

--- src/main/kotlin/example/simplelambdas/Lambdas.kt
+++ src/main/kotlin/example/simplelambdas/Lambdas.kt
@@ -1,8 +1,18 @@
 package example.simplelambdas
 
+//https://developer.android.com/codelabs/kotlin-bootcamp-sams#3
+
 val waterFilter = { dirty: Int -> dirty / 2 }
 
 fun main() {
     val result = waterFilter(30)
     println("$result")
+
+    val myFish = listOf<Fish>(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
+    val resultOfFilter = myFish.filter { it.name.contains("i") }.joinToString(", ") { it.name }
+    println("${resultOfFilter.trim()}")
 }
+
+data class Fish(val name: String)
+
+

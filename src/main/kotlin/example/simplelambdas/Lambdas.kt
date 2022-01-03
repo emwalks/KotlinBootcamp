7da1fc6a08339b718bd19cc6a7641d71da88319c
Emma Walker <walkere@tcd.ie>
2022-01-03 18:20:52 +0000

    adds new function to capitalise the first character

--- src/main/kotlin/example/simplelambdas/Lambdas.kt
+++ src/main/kotlin/example/simplelambdas/Lambdas.kt
@@ -21,9 +21,16 @@ data class Fish(val name: String)
 
 fun fishExamples() {
     val fish = Fish("splashy")
-    with(fish.name) {
-        println(this.capitalize())
-    }
+//    with(fish.name) {
+//        println(this.capitalize())
+//    }
+    myWith(fish.name) {
+        println(this.capitalize()) }
 }
 
-
+// Inside myWith(), block() is now an extension function of String.
+// The class being extended is often called the receiver object.
+// So name is the receiver object in this case
+fun myWith(name: String, block: String.() -> Unit ) {
+    name.block()
+}
